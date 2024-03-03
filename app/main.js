408b3ae11c03d74cf052ba4c6f4504224d638d97
Nathan Brown <nathanejbrown@gmail.com>
2024-03-03 14:48:15 -0600

    creates boilerplate code for routes and sets up chat connection with gemini

---
+++ app/main.js
@@ -0,0 +1,43 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const readline = require('readline');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+let model;
+let chat;
+
+async function connectToGemini() {
+  model = genAI.getGenerativeModel({ model: "gemini-pro"});
+  chat = model.startChat({});
+
+  askQuestion();
+}
+
+async function run(msg) {
+  const result = await chat.sendMessageStream(msg);
+  let text = '';
+  for await (const chunk of result.stream) {
+    const chunkText = chunk.text();
+    console.log(chunkText);
+    text += chunkText;
+  }
+  askQuestion();
+}
+
+connectToGemini();
+
+function askQuestion() {
+  rl.question('Please enter your prompt: ', (prompt) => {
+    if (prompt === 'exit') {
+      rl.close();
+    } else {
+      run(prompt)
+    }
+  });
+}
